3b9ae40ad6b2667bc6ae328788c7bf3d4df18b64
Nicky Harpor <nickyharpor@mail.ru>
2022-12-16 02:09:24 +0330

    v1.0.0

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import HCaptcha from '@hcaptcha/react-hcaptcha';
-import logo from './hCaptchaLogo.svg';
+import logo from './telegramLogo.svg';
+import TelegramLoginButton from 'telegram-login-button';
 
-function HCaptchaValidator({
+function TelegramValidator({
   renderLogoOnly = false,
   data,
   onVerify,
   validatorApiUrl,
-  sitekey,
+  minimalScore,
   ...rest
 }) {
-  const handleVerification = async (token) => {
+  const handleVerification = async (user) => {
     try {
-      const response = await fetch(validatorApiUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          data,
-          token
-        })
-      });
+      const proofUrl = validatorApiUrl + "/get_proof/" + user.id + "/" + minimalScore + "/" + data;
+      const response = await fetch(proofUrl);
       if (response.ok) {
+        await fetch(validatorApiUrl + "/send_message/" + user.id + "/" + 2);
         const { proof } = await response.json();
         onVerify({ error: false, errorMessage: null, proof });
       } else {
+        await fetch(validatorApiUrl + "/send_message/" + user.id + "/" + 1);
         const errorMessage = `${response.status} ${response.statusText}`;
         onVerify({ error: true, errorMessage, proof: null });
       }
@@ -34,18 +30,18 @@ function HCaptchaValidator({
 
   // Logo is returned in order to display it in validator selector list
   if (renderLogoOnly) {
-    return <img src={logo} alt="hCaptcha logo" {...rest} />;
+    return <img src={logo} alt="Telegram logo" {...rest} />;
   }
 
   // Main validator component
   return (
-    <HCaptcha
-      size="compact"
-      sitekey={sitekey}
-      onVerify={handleVerification}
-      {...rest}
-    />
+      <TelegramLoginButton
+          botName="humangram_bot"
+          dataOnauth={(user) => handleVerification(user)}
+          requestAccess=true
+          {...rest}
+      />
   );
 }
 
-export default HCaptchaValidator;
+export default TelegramValidator;
